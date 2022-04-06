d5211b3c0f259b189729ccbd3866b069d0b07e8f
Ortegago3 <72822704+Ortegago3@users.noreply.github.com>
2022-04-06 15:52:47 -0500

    Update images.php

--- Cloudy/images.php
+++ Cloudy/images.php
@@ -3,8 +3,9 @@
     include 'admin/config/db.php';
     $conn = conexion();
     $archivos = "SELECT * FROM archivos WHERE usuario='correo@correo.com'";
-?>
 
+
+?>
     <article class="archivos">
         <div class="boton-subir">
             <a href="subir.php">
@@ -17,11 +18,32 @@
         <div class="archivos-usuario container">
             <?php $resultado = mysqli_query($conn, $archivos);
 
-            while($row=mysqli_fetch_assoc($resultado)) {
+            while($row = mysqli_fetch_assoc($resultado)) {
+                if($row['tipo'] == 'image/png' || $row['tipo'] == 'image/jpeg'){
+                    $tipo = $row
             ?>
-           <div><?php echo $row["ruta"] ?></div>
-            <?php } mysqli_free_result($resultado);?>
+           <div overflow='auto'>
+               <?php
+                    echo "<a href='descargarFile.php?file=" .$row['ruta']. "' class='btn btn-info btn-block'>";
+                    ## ICONO PNG
+                    if ($row['tipo'] == 'image/png'){
+                        echo "<img src='Archivos/".$row['ruta']."' class='archivo'>";
+                    }
+                    ## ICONO JPEG
+                    else if ($row['tipo'] == 'image/jpeg'){
+                        echo "<img src='Archivos/".$row['ruta']."' class='archivo'>";
+                    }
+
+                    
+                    echo "</a>";
+                    echo $row["ruta"];
+            
+                ?>
+           </div>
+            <?php 
+                }
+            } mysqli_free_result($resultado);?>
         </div>
-    </article>   
+    </article>    
 
 <?php include("templates/pie.php"); ?>
