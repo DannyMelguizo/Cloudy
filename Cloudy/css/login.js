5f89b703eb4b77facae79dbbdc29013db91975e1
DanyMelguizo <73504424+Danymelguizo@users.noreply.github.com>
2022-04-24 20:30:22 -0500

    Add files via upload

---
+++ Cloudy/css/login.js
@@ -0,0 +1,58 @@
+$("#enviarLogin").click(function(event){
+    event.preventDefault();
+
+    //Almacenamos el usuario en una variable
+    var usuario = $('#usuario2').val();
+
+    //Almacenamos la contraseña en una variable
+    var password =  $('#password2').val();
+
+    //Si el usuario deja algun campo vacio, imprime una alerta
+    if(usuario.length == '' || password.length == ''){
+        Swal.fire({
+            icon: 'warning',
+            title: 'Debe ingresar un Correo y/o Contraseña'
+        })
+    } else {
+
+        //Conexion con la base de datos, mediante jQuery -- ajax
+        jQuery.ajax({
+            url: 'db/login.php',
+            type: 'POST',
+            data: {usuario: usuario, password: password},
+            beforeSend: function(){
+
+            }
+        })
+        .done(function(respuesta){
+
+            //Si el usuario se encuentra registrado, imprime un mensaje y redirige
+            if(respuesta == "No"){
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Conexion exitosa',
+                    showConfirmButton: false,
+                    timer: 1000
+                }).then(setTimeout(function(){
+                    window.location.href = "index.php"
+                    }, 1500)
+                )                
+
+            } 
+            //Si el usuario no se encuentra registrado, imprime un error
+            else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Correo y/o Contraseña incorrectas.'
+
+                });
+            }
+        })
+        .fail(function(resp){
+            console.log(resp.responseText);
+        })
+        .always(function(){
+            console.log('complete');
+        });
+    }
+});
--- Cloudy/css/redireccionar.js
+++ Cloudy/css/redireccionar.js
@@ -1,11 +1,14 @@
+
+//Imprime notificacion de archivo subido correctamente
 Swal.fire({
-    position: 'bot-end',
+    position: 'bottom-end',
     icon: 'success',
     title: 'Archivo subido correctamente.',
     showConfirmButton: false,
     timer: 1500
 })
 
+//Redirige al usuario a la pantalla de inicio
 setTimeout(function(){
     window.location.href = "index.php"
 }, 2500);
